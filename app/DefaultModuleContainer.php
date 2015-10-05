c3b30950e19591a911aaec40cf03cde86db04f5f
Butschster <butschster@gmail.com>
2015-10-05 16:27:50 +0300

    Расширение поведения стандартного загрузчика модулей

---
+++ app/DefaultModuleContainer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use KodiCMS\ModulesLoader\ModuleContainer as BaseModuleContainer;
+
+class DefaultModuleContainer extends BaseModuleContainer
+{
+
+	/**
+	 * @param \Illuminate\Foundation\Application $app
+	 *
+	 * @return $this
+	 */
+	public function boot($app)
+	{
+		if ( ! $this->isBooted) {
+			$this->loadViews();
+			$this->loadTranslations();
+			$this->loadAssets();
+			$this->isBooted = true;
+		}
+
+		return $this;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getAssetsPackagesPath()
+	{
+		return $this->getPath([ 'resources', 'packages.php' ]);
+	}
+
+
+	protected function loadAssets()
+	{
+		if (is_file($packagesFile = $this->getAssetsPackagesPath())) {
+			require $packagesFile;
+		}
+	}
+}
