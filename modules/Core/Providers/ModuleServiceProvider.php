e89da1ce03d8b403139fb4723761b522c2be57bb
Butschster <butschster@gmail.com>
2015-10-07 21:02:13 +0300

    Консольные команды вынесены в Загрузчик модулей

--- modules/Core/Providers/ModuleServiceProvider.php
+++ modules/Core/Providers/ModuleServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace Modules\Core\Providers;
 
-use Modules\Core\Console\Commands\ModulesSeedCommand;
 use Modules\Core\Console\Commands\DropDatabaseCommand;
-use Modules\Core\Console\Commands\ModulesMigrateCommand;
 use KodiCMS\ModulesLoader\Providers\ModuleServiceProvider as BaseServiceProvider;
 
 class ModuleServiceProvider extends BaseServiceProvider
@@ -18,7 +16,5 @@ class ModuleServiceProvider extends BaseServiceProvider
     public function register()
     {
         $this->registerConsoleCommand('DropDatabaseCommand', DropDatabaseCommand::class);
-        $this->registerConsoleCommand('ModulesMigrateCommand', ModulesMigrateCommand::class);
-        $this->registerConsoleCommand('ModulesSeedCommand', ModulesSeedCommand::class);
     }
 }
