f1d4121dbfe79dd515a98946abbe6509c4921142
Butschster <butschster@gmail.com>
2015-10-07 20:25:05 +0300

    fix php syntax

--- app/DefaultModuleContainer.php
+++ app/DefaultModuleContainer.php
@@ -7,37 +7,37 @@ use KodiCMS\ModulesLoader\ModuleContainer as BaseModuleContainer;
 class DefaultModuleContainer extends BaseModuleContainer
 {
 
-	/**
-	 * @param \Illuminate\Foundation\Application $app
-	 *
-	 * @return $this
-	 */
-	public function boot($app)
-	{
-		if ( ! $this->isBooted) {
-			$this->loadViews();
-			$this->loadTranslations();
-			$this->loadAssets();
-			$this->isBooted = true;
-		}
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return $this
+     */
+    public function boot($app)
+    {
+        if ( ! $this->isBooted) {
+            $this->loadViews();
+            $this->loadTranslations();
+            $this->loadAssets();
+            $this->isBooted = true;
+        }
 
-		return $this;
-	}
-
-
-	/**
-	 * @return string
-	 */
-	public function getAssetsPackagesPath()
-	{
-		return $this->getPath([ 'resources', 'packages.php' ]);
-	}
+        return $this;
+    }
 
 
-	protected function loadAssets()
-	{
-		if (is_file($packagesFile = $this->getAssetsPackagesPath())) {
-			require $packagesFile;
-		}
-	}
+    /**
+     * @return string
+     */
+    public function getAssetsPackagesPath()
+    {
+        return $this->getPath(['resources', 'packages.php']);
+    }
+
+
+    protected function loadAssets()
+    {
+        if (is_file($packagesFile = $this->getAssetsPackagesPath())) {
+            require $packagesFile;
+        }
+    }
 }
