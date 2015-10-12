e52977c9e2dadd95393b95ff0a86407063bf9128
Butschster <butschster@gmail.com>
2015-10-12 13:38:44 +0300

    Доработка gulpfile.js
    Добавлена версионность для app.js.
    Файл со стилями all.css переименован в app.css
    
    Поправлено форматирование

--- gulpfile.js
+++ gulpfile.js
@@ -6,38 +6,38 @@ var paths = {
     'assetsCss': elixir.config.assetsPath + '/css/',
     'assetsSass': elixir.config.assetsPath + '/sass/',
     'assetsJs': elixir.config.assetsPath + '/js/',
-    'bootstrap':    './node_modules/bootstrap-sass/assets/',
-    'jquery':       './bower_components/jquery/',
-    'underscore':   './bower_components/underscore/',
-    'datetimepicker':'./bower_components/datetimepicker/',
-    'fontawesome':'./bower_components/fontawesome/',
-    'jqueryvalidation':'./bower_components/jquery-validation/',
+    'bootstrap': './node_modules/bootstrap-sass/assets/',
+    'jquery': './bower_components/jquery/',
+    'underscore': './bower_components/underscore/',
+    'datetimepicker': './bower_components/datetimepicker/',
+    'fontawesome': './bower_components/fontawesome/',
+    'jqueryvalidation': './bower_components/jquery-validation/',
     'jquerytagsinput': './bower_components/jquery.tagsinput/',
-    'noty':'./bower_components/noty/',
-    'select2':'./bower_components/select2/'
+    'noty': './bower_components/noty/',
+    'select2': './bower_components/select2/'
 };
 
-elixir(function(mix) {
 
+elixir(function (mix) {
     mix
-        //SaSS
+        // SaSS
         .sass('bootstrap.scss', paths.assetsCss)
         .sass('app.scss', paths.assetsCss)
-        //CSS
+
+        // CSS
         .styles([
             paths.assetsCss + 'bootstrap.css',
             paths.select2 + "dist/css/select2.css",
             paths.datetimepicker + "jquery.datetimepicker.css",
             paths.jquerytagsinput + "src/jquery.tagsinput.css",
             paths.assetsCss + 'app.css'
-        ])
-        .version('public/css/all.css');
-        //Fonts
-    mix
+        ], 'public/css/app.css')
+
+         // Fonts
         .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
         .copy(paths.fontawesome + 'fonts/**', 'public/fonts')
 
-        //JavaScript
+        // JavaScript
         .scripts([
             paths.jquery + 'dist/jquery.js',
             paths.bootstrap + "javascripts/bootstrap.js"
@@ -49,5 +49,8 @@ elixir(function(mix) {
             'components.js',
             'controllers/articles.js',
             'run.js'
-        ], 'public/js/app.js');
+        ], 'public/js/app.js')
+
+        // Versioning
+        .version(['public/css/app.css', 'public/js/app.js']);
 });
