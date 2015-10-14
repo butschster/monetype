6b96559d36cf53ee878d20eec63956c9a8c96886
butschster <butschster@gmail.com>
2015-10-14 17:08:40 +0300

    Create Article jobs

---
+++ modules/Articles/Jobs/DraftArticle.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Modules\Articles\Jobs;
+
+use Modules\Users\Model\User;
+use Modules\Articles\Model\Article;
+use Illuminate\Contracts\Bus\SelfHandling;
+
+class DraftArticle implements SelfHandling
+{
+
+    /**
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * @var Article
+     */
+    protected $article;
+
+    /**
+     * @var bool
+     */
+    protected $notify = true;
+
+
+    /**
+     * @param User|null $user
+     * @param Article   $article
+     * @param bool|true $notify
+     */
+    public function __construct(User $user, Article $article, $notify = true)
+    {
+        $this->user    = $user;
+        $this->article = $article;
+        $this->notify  = (bool) $notify;
+    }
+
+
+    public function handle()
+    {
+        $this->article->setDraft();
+    }
+}
