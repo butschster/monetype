0def8b2bb338b5dccad07485215c41ccc23403be
Butschster <butschster@gmail.com>
2015-10-15 17:45:41 +0300

    Add new media libraries

--- modules/Core/resources/packages.php
+++ modules/Core/resources/packages.php
@@ -6,3 +6,11 @@ Package::add('libraries')
 Package::add('app')
 	->css(null, url('css/app.css'))
 	->js(null, url('js/app.js'), 'libraries');
+
+Package::add('dropzone')
+	   ->css('dropzone.basic', url('libs/dropzone/basic.min.css'))
+	   ->css(null, url('libs/dropzone/dropzone.min.css'))
+	   ->js(null, url('libs/dropzone/dropzone.min.js'), 'libraries');
+
+Package::add('backstretch')
+	   ->js(null, url('libs/jquery.backstretch/jquery.backstretch.min.js'), 'libraries');
