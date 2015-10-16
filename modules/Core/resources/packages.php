d97a1342578220709facae7009583b5d416ead45
butschster <butschster@gmail.com>
2015-10-16 18:26:25 +0300

    fix php syntax

--- modules/Core/resources/packages.php
+++ modules/Core/resources/packages.php
@@ -1,16 +1,16 @@
 <?php
 
 Package::add('libraries')
-	->js(null, url('js/lib.js'));
+        ->js(null, url('js/lib.js'));
 
 Package::add('app')
-	->css(null, url('css/app.css'))
-	->js(null, url('js/app.js'), 'libraries');
+        ->css(null, url('css/app.css'))
+        ->js(null, url('js/app.js'), 'libraries');
 
 Package::add('dropzone')
-	   ->css('dropzone.basic', url('libs/dropzone/basic.min.css'))
-	   ->css(null, url('libs/dropzone/dropzone.min.css'))
-	   ->js(null, url('libs/dropzone/dropzone.min.js'), 'libraries');
+        ->css('dropzone.basic', url('libs/dropzone/basic.min.css'))
+        ->css(null, url('libs/dropzone/dropzone.min.css'))
+        ->js(null, url('libs/dropzone/dropzone.min.js'), 'libraries');
 
 Package::add('backstretch')
-	   ->js(null, url('libs/jquery.backstretch/jquery.backstretch.min.js'), 'libraries');
+        ->js(null, url('libs/jquery.backstretch/jquery.backstretch.min.js'), 'libraries');
