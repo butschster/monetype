19aeee1b5633221ccbc8e44fbbe04ef356a8c4ad
Butschster <butschster@gmail.com>
2015-10-20 20:28:16 +0300

    Доработана страница со счетчиком

--- modules/Core/resources/views/comingsoon/index.blade.php
+++ modules/Core/resources/views/comingsoon/index.blade.php
@@ -1,35 +1,53 @@
 @extends('core::layout.empty')
 
 @section('content')
-    <div class="header-container">
-        <div class="container">
-            {!! HTML::image('img/logo.gif', null, ['class' => 'header-logo']) !!}
-            <p class="lead">@lang('core::comingsoon.aboutProject')</p>
-        </div>
-        <div class="coming-soon-border"></div>
-    </div>
     <div class="cooming-soon-content">
-        <div class="coming-soon-bg-cover"></div>
-        <div class="container">
-            <div class="row coming-soon">
-                <div class="col-md-7 col-sm-5 text text-left">
-                    <h3 style="margin-top: -10px;">@lang('core::comingsoon.howWorksTitle')</h3>
-                    <p>@lang('core::comingsoon.howWorksText')</p>
-                </div>
-                <div class="col-md-5 col-sm-7 form-box wow fadeInUp animated" style="visibility: visible;">
-                    @if(auth()->check())
-                        <div class="well">
-                            <div class="headline">
-                                <h3>@lang('core::comingsoon.thankRegistration')</h3>
-                            </div>
+        <div class="container header-container">
+            {!! HTML::image('img/logo.gif', null, ['class' => 'header-logo']) !!}
 
-                            <hr />
-                            <p>@lang('core::comingsoon.startBalance', ['balance' => $currentUser->balance])</p>
-                            <p>@lang('core::comingsoon.startNotification')</p>
+        </div>
+
+        <div class="coming-soon-bg-cover"></div>
+
+        <div class="container">
+            <div class="coming-soon">
+                <div class="text">
+                    <h2 class="m-t-none m-b-lg">@lang('core::comingsoon.aboutProject')</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-7 text text-left">
+                        <h3 style="margin-top: -10px;">@lang('core::comingsoon.howWorksTitle')</h3>
+                        <p>@lang('core::comingsoon.howWorksText')</p>
+
+                        <div class="hidden-sm hidden-xs">
+                            <br /><br /><br /><br />
                         </div>
-                    @else
-                        @include('users::auth.partials.registerForm')
-                    @endif
+
+                        <h3>@lang('core::comingsoon.startProject')</h3>
+                        <div class="coming-soon-plugin">
+                            <div id="defaultCountdown"></div>
+                        </div>
+
+                        <div class="visible-sm visible-xs">
+                            <br /><br />
+                        </div>
+                    </div>
+                    <div class="col-md-5 wow fadeInUp animated" style="visibility: visible;">
+                        @if(auth()->check())
+                            <div class="well">
+                                <div class="headline">
+                                    <h3>@lang('core::comingsoon.thankRegistration')</h3>
+                                </div>
+
+                                <hr />
+                                <p>@lang('core::comingsoon.startBalance', ['balance' => $currentUser->balance])</p>
+                                <p>@lang('core::comingsoon.startNotification')</p>
+                            </div>
+                        @else
+                            @include('users::auth.partials.registerForm')
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
@@ -37,11 +55,7 @@
     <div class="coming-soon-border"></div>
     <div class="section-container section-container-gray-bg">
         <div class="container">
-            <h2 class="text-center">@lang('core::comingsoon.startProject')</h2>
 
-            <div class="coming-soon-plugin">
-                <div id="defaultCountdown"></div>
-            </div>
 
         </div>
     </div>
