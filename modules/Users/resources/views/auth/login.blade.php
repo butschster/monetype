b71014acc56c981995edd9c93e0f04aa5ca9e7c4
Butschster <butschster@gmail.com>
2015-10-24 00:05:52 +0300

    Fix icon

--- modules/Users/resources/views/auth/login.blade.php
+++ modules/Users/resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
 			<div class="panel-body">
 				<div class="form-group">
 					<div class="input-icon-append">
-						<i class="icon-append icon-envelope-o"></i>
+						<i class="icon-append icon-mail"></i>
 						{!! Form::email('email', null, ['class' => 'form-control', 'placeholder' => trans('users::user.field.email')]) !!}
 					</div>
 				</div>
